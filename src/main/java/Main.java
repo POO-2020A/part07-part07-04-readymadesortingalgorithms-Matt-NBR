d166cf16f349f2bbb38ffd46e98ecbef040511fe
Matt-NBR <66144847+Matt-NBR@users.noreply.github.com>
2020-08-02 19:05:31 -0500

    Add files via upload

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,35 @@
 
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) {
         // insert test code here
-        int[] array = {3, 1, 5, 99, 3, 12};
+        int[] intArray = {3, 1, 5, 99, 3, 12};
+        sort(intArray);
+        System.out.println("");
+        System.out.println(Arrays.toString(intArray));
+        String[] strArray = {"hey", "hello", "world", "!"};
+        sort(strArray);
+        System.out.println(Arrays.toString(strArray));
+    }
+    
+    public static void sort(int[] array) {
+        Arrays.sort(array);
+    }
+    
+    public static void sort(String[] array) {
+        Arrays.sort(array);
+    }
+    
+    public static void sortIntegers(ArrayList<Integer> integers) {
+        Collections.sort(integers);
+    }
+    
+    public static void sortStrings(ArrayList<String> strings) {
+        Collections.sort(strings);
     }
 
 }
